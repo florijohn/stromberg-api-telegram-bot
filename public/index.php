d46f354525b6ca391ff543fed35a28bb661a40a8
Floris <65296458+florijohn@users.noreply.github.com>
2022-08-21 21:57:49 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,16 +1,21 @@
 <?php
 // Includde the autoloader
 include '../vendor/autoload.php';
+require '../config/config.php';
 // Load Config
 $jsonConfig = file_get_contents("../config/config.json");
 $config = json_decode($jsonConfig, true);
 
 
-$telegram = new Telegram($config->telegramToken);
+$telegram = new Telegram($telegramAuth);
 $chat_id = $telegram->ChatID();
 $text = $telegram->Text();
 
-if ($text == '/zitat@strombergapi_bot') {
+$groupTitle = $telegram->messageFromGroupTitle();
+$groupTitle = $groupTitle ? $groupTitle : "";
+
+// TODO
+if ($text == "/zitat . $groupTitle") {
     $text = getRandomZitat();
     $content = ['chat_id' => $chat_id, 'photo' => 'https://assets.deutschlandfunk.de/FILE_96f808ea3505c4ae16aac5e51d4ebba2/1920x1080.jpg?t=1597584255401', 'caption' => $text];;
     $telegram->sendPhoto($content);
